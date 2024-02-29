9b10a1519a5c9c5d2b3e0cd9cdb2315249ab61eb
faresAlharbi0 <2040846@uj.edu.sa>
2024-02-29 19:06:25 +0300

    added welcome page - fares

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 app.use("/",express.static("./website"));
+const path = require('path');
 app.listen(2500,(req,res)=>{
     // type "npm run dev" on the terminal for the server to run with hot reloading
     console.log("server started at 2500")
 });
+
+app.get("/",(req,res) => {
+    res.sendFile(path.join(__dirname, 'app', 'welcome.html'));
+})
