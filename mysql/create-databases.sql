a78197eceaa06ddc3cfc16121bb16591518abcb5
faresAlharbi0 <2040846@uj.edu.sa>
2024-04-29 00:16:14 +0300

    fixes

--- mysql/create-databases.sql
+++ mysql/create-databases.sql
@@ -9,6 +9,6 @@ CREATE TABLE IF NOT EXISTS users (
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    bio TEXT,
+    bio VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
