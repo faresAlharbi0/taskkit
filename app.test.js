914bf49f6d59e390baa514652f2bbb487af23741
faresAlharbi0 <2040846@uj.edu.sa>
2024-05-18 19:10:06 +0300

    fix

--- app.test.js
+++ app.test.js
@@ -1,6 +1,5 @@
 const request = require('supertest');
 const server = require('./server');
-
 afterAll(() => {
     // Close the server after all tests are done
     server.close();
@@ -12,5 +11,21 @@ describe('work space retrival set', () => {
         .get('/myws/'+"@hf")
   
       expect(response.status).toBe(401);
+    });
+    it('should return a 200 status code for a valid form submission', async () => {
+        const response = await request(server)
+          .get('/myws/'+"@fares")
+    
+        expect(response.status).toBe(200);
+      });
+});
+
+describe('work space retrival set', () => {
+    it('should return a 200 status code for a valid form submission', async () => {
+      const response = await request(server)
+        .post("/addws")
+        .send({username:"@fares", workspaceName:"my workspace", workspaceDescription:"description"})
+  
+      expect(response.status).toBe(201);
     });
 });
